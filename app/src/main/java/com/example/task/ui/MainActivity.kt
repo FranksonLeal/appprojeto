0e9d952bc6762d9e73e4106eab24824103c2392f
Frankson Leal <frankson.11leal@gmail.com>
2024-05-16 19:56:15 -0300

    atualizacao aula

---
+++ app/src/main/java/com/example/task/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.task.ui
+
+import android.os.Binder
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.task.R
+import com.example.task.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+}
