88e1038745767df93774a58a37351bf0d1f483e8
Frankson Leal <frankson.11leal@gmail.com>
2024-05-22 20:13:13 -0300

    atualizacao

--- app/src/main/java/com/example/task/ui/SplashFragment.kt
+++ app/src/main/java/com/example/task/ui/SplashFragment.kt
@@ -1,30 +1,47 @@
 package com.example.task.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.task.R
+import com.example.task.databinding.FragmentSplashBinding
 
 // TODO: Rename parameter arguments, choose names that match
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SplashFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    private var _binding: FragmentSplashBinding? = null
+    private  val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 3000)
+    }
+
+    //função para checar se o usuario esta autenticado ou nao
+    private fun checkAuth(){
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
